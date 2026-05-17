955cda1edea4ed45e00821073736d0f90b202086
BerndU-2055267 <bernd.uijtdebroeks@student.uhasselt.be>
2026-05-17 14:55:28 +0200

    Secure Coding #3 en #4
    Secure Coding oplossingen voor deel 3 en 4
    (de oplossingen voor deel 1 en 2 staan in de repository van Koray Zevne)

--- backend/index.js
+++ backend/index.js
@@ -5,39 +5,46 @@ const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors')
 
-const port=3000;
+const port = 3000;
 
 const pool = new pg.Pool({
-    user: 'secadv',
-    host: 'db',
-    database: 'pxldb',
-    password: 'ilovesecurity',
-    port: 5432,
-    connectionTimeoutMillis: 5000
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT, 10),
+  connectionTimeoutMillis: 5000
 })
 
 console.log("Connecting...:")
 
-app.use(cors());
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type'],
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(
-    bodyParser.urlencoded({
-        extended: true,
-    })
+  bodyParser.urlencoded({
+    extended: true,
+  })
 )
 
 app.get('/authenticate/:username/:password', async (request, response) => {
-    const username = request.params.username;
-    const password = request.params.password;
+  const username = request.params.username;
+  const password = request.params.password;
 
-    const query = `SELECT * FROM users WHERE user_name='${username}' and password='${password}'`;
-    console.log(query);
-    pool.query(query, (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).json(results.rows)});
-      
+  const query = `SELECT * FROM users WHERE user_name='${username}' and password='${password}'`;
+  console.log(query);
+  pool.query(query, (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  });
+
 });
 
 app.listen(port, () => {
